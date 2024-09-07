649cd659ad0035b235420aaca0a20551f6fbc429
jimzhang <zhangjing.76@outlook.com>
2024-09-07 21:18:03 +1000

    Completed hw0.java

---
+++ cs61class/wk1/hw0.java
@@ -0,0 +1,9 @@
+public class ClassNameHere {
+   public static void main(String[] args) {
+      int x = 1;
+      while (x < 6) {
+        System.out.print("*"*x);
+        System.out.println();
+      } 
+   }
+}
