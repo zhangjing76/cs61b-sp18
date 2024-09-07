d369aedd79f172f395ab0b07bbbc4e665873293b
jimzhang <zhangjing.76@outlook.com>
2024-09-07 21:22:25 +1000

    hw0.java

--- cs61class/wk1/hw0.java
+++ cs61class/wk1/hw0.java
@@ -1,9 +1,15 @@
 public class ClassNameHere {
    public static void main(String[] args) {
       int x = 1;
+      int y = 0;
       while (x < 6) {
-        System.out.print("*"*x);
+        y = 0;
+        while (y < x) {
+            System.out.print("*");
+            y = y + 1;
+        }
         System.out.println();
+        x = x + 1;
       } 
    }
 }
