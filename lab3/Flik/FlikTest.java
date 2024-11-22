6be7aaf4c65276f7e7d8c51a85f93deeadc2bbaf
jimzhang <zhangjing.76@outlook.com>
2024-11-23 09:58:52 +1100

    Fixed indentation and style

--- lab3/Flik/FlikTest.java
+++ lab3/Flik/FlikTest.java
@@ -1,20 +1,19 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 public class FlikTest {
-        /** Tests Flik*/
-        @Test
-        public void testFlik() {
-            int a = 5;
-            int b = -8;
-            boolean expected = false;
-            boolean actual = Flik.isSameNumber(a, b);
-            assertEquals(expected, actual);
+    /** Tests Flik*/
+    @Test
+    public void testFlik() {
+        int a = 5;
+        int b = -8;
+        boolean expected = false;
+        boolean actual = Flik.isSameNumber(a, b);
+        assertEquals(expected, actual);
 
-            b = 5;
-            assertTrue(Flik.isSameNumber(a, b));
-        }
+        b = 5;
+        assertTrue(Flik.isSameNumber(a, b));
+    }
 }
